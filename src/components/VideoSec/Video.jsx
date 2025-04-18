27836534fed7581dbfc2dccc447cdb81e324b8d2
Debayan-Sarkar <debayansarkar368@gmail.com>
2025-04-19 01:33:02 +0530

    video

--- src/components/VideoSec/Video.jsx
+++ src/components/VideoSec/Video.jsx
@@ -4,7 +4,7 @@ import { MdArrowCircleDown } from 'react-icons/md'
 const Video = () => {
     return (
         <section className="hero flex flex-col items-center justify-center h-[90vh] !pl-24 !pr-24">
-            <video width={'100%'} height={'100%'} controls={false} autoPlay loop>
+            <video width={'100%'} height={'100%'} controls={false} autoPlay loop muted playsInline preload="auto">
                 <source src="/assets/intro-video.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
