529a9521b18ae40e88ddc68e33962aa5d1cf6131
Debayan-Sarkar <debayansarkar368@gmail.com>
2025-04-26 16:01:04 +0530

    m

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,23 +11,47 @@ import Image from "next/image";
 import { useEffect, useRef } from "react";
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import SplitType from "split-type";
 
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   const preloaderRef = useRef();
+
+
   useEffect(() => {
     preloaderRef.current?.startAnimation();
-  
+
     const works = gsap.utils.toArray('.workSec');
-  
+    const acheved = gsap.utils.toArray('.acheved');
+    const SplitTexts = gsap.utils.toArray('.fill-text');
+    SplitTexts.forEach((text) => {
+      const split = new SplitType(text, { types: 'chars' });
+      console.log(split);
+
+
+      gsap.from(split.chars, {
+        opacity: 0.1,
+        y: 20,
+        scrollTrigger: {
+          trigger: text,
+          start: 'top center',
+          end: `+=${window.innerHeight}`,
+          scrub: true,
+          markers: false
+        },
+        duration: 0.5,
+        stagger: { amount: 0.1 },
+      })
+    }
+    );
     works.forEach((work, index) => {
       const image = work.querySelector('.img img');
       const info = work.querySelector('.info');
-  
+
       if (!image || !info) return;
-  
+
       gsap.from(image, {
         x: index % 2 === 0 ? 0.5 * image.clientWidth : -0.5 * image.clientWidth,
         rotate: index % 2 === 0 ? 10 : -10,
@@ -40,9 +64,9 @@ export default function Home() {
           markers: false
         }
       });
-  
+
       gsap.set(info, { yPercent: -5 });
-  
+
       gsap.from(info, {
         yPercent: 55,
         duration: 2,
@@ -56,6 +80,66 @@ export default function Home() {
         }
       });
     });
+
+
+
+
+    let r = document.querySelector(".acheved");
+    let r1 = document.querySelector(".acheved1");
+    let r2 = document.querySelector(".acheved2");
+    let r3 = document.querySelector(".acheved3");
+
+    gsap.from(r, {
+      x: "-100%",
+      rotate: -45,
+      delay: 0.5,
+      ease: 'power1.in',
+      scrollTrigger: {
+        trigger: r,
+        start: 'top center',
+        end: `+=${window.innerHeight}`,
+        scrub: true,
+        markers: false
+      }
+    });
+    gsap.from(r1, {
+      x: "100%",
+      rotate: 45,
+      ease: 'power1.in',
+      scrollTrigger: {
+        trigger: r1,
+        start: 'top center',
+        end: `+=${window.innerHeight}`,
+        scrub: true,
+        markers: false
+      }
+    });
+    gsap.from(r2, {
+      x: "-100%",
+      rotate: -45,
+      delay: 2,
+      ease: 'power1.in',
+      scrollTrigger: {
+        trigger: r,
+        start: 'top center',
+        end: `+=${window.innerHeight}`,
+        scrub: true,
+        markers: false
+      }
+    });
+    gsap.from(r3, {
+      x: "100%",
+      rotate: 45,
+      ease: 'power1.in',
+      scrollTrigger: {
+        trigger: r3,
+        start: 'top center',
+        end: `+=${window.innerHeight}`,
+        scrub: true,
+        markers: false
+      }
+    });
+
   }, []);
 
   return (
@@ -67,11 +151,12 @@ export default function Home() {
           <Hero />
           <Video />
           <section className="!mt-65 !pr-24 !pl-24 text-white">
-            <div className="flex flex-col">
-              <div className="top">
-                <span className="text-[218px] leading-40 block"> recent </span>
-                <span className="text-[218px] !ml-1.5 leading-40 block"> work</span>
+            <div className="flex flex-col scrfff">
+              <div className="RecWork">
+                <span className="fill-text block text-[218px] leading-40">recent</span>
+                <span className="fill-text block text-[218px] leading-40 ml-1.5">work</span>
               </div>
+
               <div className="bottom flex w-full justify-between items-center">
                 <div className="">
                   <span className="block syne text-3xl">In the creative wilderness, </span>
@@ -87,8 +172,8 @@ export default function Home() {
                 </button>
               </div>
             </div>
-            <div className="workSec flex justify-between items-center !mt-30">
-              <div className="info w-[50%]">
+            <div className="workSec flex justify-between items-center !mt-28 gap-2.5">
+              <div className="info w-[40%]">
                 <h1 className="text-7xl">ovrmelt</h1>
                 <h4 className="syne text-xl">Social Media Revamp</h4>
                 <button className="snakeBorder syne heroBtn !p-4 w-43 rounded-full transition-all duration-300 ease-linear cursor-pointer">
@@ -99,15 +184,15 @@ export default function Home() {
                   Explore Work
                 </button>
               </div>
-              <div className="img w-[50%]">
+              <div className="img w-[60%]">
                 <Image src={'/assets/work.webp'} className="w-full rounded-2xl" width={100} height={100} alt="Work Images" />
               </div>
             </div>
-            <div className="workSec flex justify-between items-center">
-              <div className="img w-[50%]">
+            <div className="workSec flex justify-between items-center !mt-28 gap-2.5">
+              <div className="img w-[60%]">
                 <Image src={'/assets/work2.webp'} className="w-full rounded-2xl" width={100} height={100} alt="Work Images" />
               </div>
-              <div className="info text-end w-[50%]">
+              <div className="info text-end w-[40%]">
                 <h1 className="text-7xl">kothari construction
                   company</h1>
                 <h4 className="syne text-xl">Lead Generation Success</h4>
@@ -120,8 +205,8 @@ export default function Home() {
                 </button>
               </div>
             </div>
-            <div className="workSec flex justify-between items-center">
-              <div className="info w-[50%]">
+            <div className="workSec flex justify-between items-center !mt-28 gap-2.5">
+              <div className="info w-[40%]">
                 <h1 className="text-7xl">vastram</h1>
                 <h4 className="syne text-xl">Complete Branding Overhaul</h4>
                 <button className="snakeBorder syne heroBtn !p-4 w-43 rounded-full transition-all duration-300 ease-linear cursor-pointer">
@@ -132,12 +217,28 @@ export default function Home() {
                   Explore Work
                 </button>
               </div>
-              <div className="img w-[50%]">
+              <div className="img w-[60%]">
                 <Image src={'/assets/work3.webp'} className="w-full rounded-2xl" width={100} height={100} alt="Work Images" />
               </div>
             </div>
+            <div className="workSec flex justify-between items-center !mt-28 gap-3">
+              <div className="img w-[60%]">
+                <Image src={'/assets/work3.webp'} className="w-full rounded-2xl" width={100} height={100} alt="Work Images" />
+              </div>
+              <div className="info w-[40%]">
+                <h1 className="text-7xl">isha jewellers</h1>
+                <h4 className="syne text-xl"></h4>
+                <button className="snakeBorder syne heroBtn !p-4 w-43 rounded-full transition-all duration-300 ease-linear cursor-pointer">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  Explore Work
+                </button>
+              </div>
+            </div>
           </section>
-          <section className="!pr-24 !pl-24 text-white">
+          <section className="!pr-24 !pl-24 text-white !mt-90">
             <div className="flex flex-col">
               <div className="top">
                 <span className="text-[218px] leading-40 block">who</span>
@@ -151,7 +252,7 @@ export default function Home() {
                 <span className="block syne text-3xl">excellence.</span>
               </div>
             </div>
-            <div className="ml-auto w-full flex flex-col">
+            <div className="ml-auto w-full flex flex-col gap-2.5">
               <span className="syne text-xl !mt-5">At Omniscient.ltd, we specialize in crafting compelling brand identities, delivering innovative marketing strategies, and creating creative designs to elevate your business.</span>
               <button className="snakeBorder syne heroBtn !p-4 w-43 rounded-full transition-all duration-300 ease-linear cursor-pointer">
                 <span></span>
@@ -162,11 +263,82 @@ export default function Home() {
               </button>
             </div>
           </section>
-          <section className="!pr-24 !pl-24">
-            recent work
+          <section className="!pr-24 !pl-24 !pb-48 !mt-50 overflow-hidden">
+            <div className="achvedCont grid grid-cols-2 !gap-6">
+              <div className="acheved bg-[#1f202266] rounded-2xl z-0 !p-14 hover:bg-amber-700 text-white transition-all duration-300 ease-linear cursor-pointer">
+                <div className="acchheWrap">
+                  <div className="nums !mb-16 text-8xl">
+                    <span>2</span>
+                    <span>0</span>
+                    <sup>+</sup>
+                  </div>
+                  <div className="flex items-center justify-end text-5xl">
+                    creative<br />
+                    designs
+                  </div>
+                </div>
+              </div>
+              <div className="acheved1 bg-[#1f202266] rounded-2xl z-0 !p-14 !mt-40 !-mb-40 hover:bg-green-500 text-white transition-all duration-300 ease-linear cursor-pointer">
+                <div className="acchheWrap">
+                  <div className="nums !mb-16 text-8xl">
+                    <span>2</span>
+                    <span>0</span>
+                    <sup>+</sup>
+                  </div>
+                  <div className="flex items-center justify-end text-5xl">
+                    creative<br />
+                    designs
+                  </div>
+                </div>
+              </div>
+              <div className="acheved2 bg-[#1f202266] rounded-2xl z-0 !p-14 hover:bg-yellow-200 text-white hover:text-black transition-all duration-300 ease-linear cursor-pointer">
+                <div className="acchheWrap">
+                  <div className="nums !mb-16 text-8xl">
+                    <span>2</span>
+                    <span>0</span>
+                    <sup>+</sup>
+                  </div>
+                  <div className="flex items-center justify-end text-5xl">
+                    creative<br />
+                    designs
+                  </div>
+                </div>
+              </div>
+              <div className="acheved3 bg-[#1f202266] rounded-2xl z-0 !p-14 !mt-40 !-mb-40 hover:bg-yellow-500 hover:text-black text-white transition-all duration-300 ease-linear cursor-pointer">
+                <div className="acchheWrap">
+                  <div className="nums !mb-16 text-8xl">
+                    <span>2</span>
+                    <span>0</span>
+                    <sup>+</sup>
+                  </div>
+                  <div className="flex items-center justify-end text-5xl">
+                    creative<br />
+                    designs
+                  </div>
+                </div>
+              </div>
+            </div>
           </section>
           <section className="!pr-24 !pl-24">
-            recent work
+            <div className="testimonialCont">
+              <div className="texths flex justify-between items-end text-white">
+                <h1 className=" !leading-24">
+                  <span className="block text-start text-[117px] ">partner</span>
+                  <span className="block text-start text-[117px] ">love</span>
+                </h1>
+                  <h4 className="syne text-2xl">
+                    <div>
+                      <div>Take heed, as the </div>
+                    </div>
+                    <div>
+                      <div>lion's roar in our client </div>
+                    </div>
+                    <div>
+                      <div>reviews resounds.</div>
+                    </div>
+                  </h4>
+              </div>
+            </div>
           </section>
           <section className="!pr-24 !pl-24">
             recent work
--- src/components/VideoSec/Video.jsx
+++ src/components/VideoSec/Video.jsx
@@ -25,13 +25,13 @@ const Video = () => {
             height: "90%",
             marginBottom: "200px",
             borderRadius: "50px",
-            ease: "none",
+            ease: "power1.in",
         });
     }, [])
     return (
         <div id="video" className="pointer-events-none flex flex-col items-center justify-end  w-full !pl-24 !pr-24  z-0">
-            <video width={'100%'} ref={videoRef} height={'100%'} controls={false} autoPlay loop muted playsInline preload="auto" className='video w-[170px] rounded-full origin-top'>
-                <source src="/assets/intro-video.mp4" type="video/mp4" />
+            <video width={'100%'} ref={videoRef} height={'100%'} controls={false} autoPlay loop muted playsInline preload="auto" className='video w-[170px] rounded-full origin-top '>
+                <source src="/assets/intro.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
         </div>
