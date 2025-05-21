3d2fba262f48a5e4331759f5f43b45c19ee46b31
Debayan-Sarkar <debayansarkar368@gmail.com>
2025-05-22 00:52:48 +0530

    m

--- ddedededfef/src/components/Functions/Functions.jsx
+++
@@ -1,6 +0,0 @@
-
-// export const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-import gsap from "gsap";
-
-export const random = (min, max, snap = "") => gsap.utils.random(min, max, snap);
